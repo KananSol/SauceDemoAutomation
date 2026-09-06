98441f680144ee5667ce6fd22d40c9cf4c1bdcd2
Kanan <Kanansoltanli@gmail.com>
2026-09-06 13:31:41 +0400

    Added waits to the tests

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -3,9 +3,15 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class InventoryPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
     private By productsText = By.className("title");
     private By addBackpackButton = By.id("add-to-cart-sauce-labs-backpack");
     private By cartBadge = By.className("shopping_cart_badge");
@@ -13,22 +19,34 @@ public class InventoryPage {
 
     public InventoryPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public String getProductsTitle() {
-        return driver.findElement(productsText).getText();
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(productsText)
+        ).getText();
     }
 
     public void addBackpackToCart() {
-        driver.findElement(addBackpackButton).click();
+        wait.until(
+                ExpectedConditions.elementToBeClickable(addBackpackButton)
+        ).click();
     }
 
     public String getCartItemCount() {
+        wait.until(
+                ExpectedConditions.textToBe(cartBadge, "1")
+        );
+
         return driver.findElement(cartBadge).getText();
     }
 
     public void openCart() {
-        driver.findElement(cartLink).click();
+        wait.until(
+                ExpectedConditions.elementToBeClickable(cartLink)
+        ).click();
     }
+
 
 }
